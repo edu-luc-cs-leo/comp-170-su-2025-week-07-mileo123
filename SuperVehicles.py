fcf97213bdeaad565abd312946dbd892cf36fcc3
mileo123 <mgray12@luc.edu>
2025-07-16 14:28:06 -0500

    Update SuperVehicles.py

--- SuperVehicles.py
+++ SuperVehicles.py
@@ -1,4 +1,30 @@
+class Vehicle:
+  def __init__(self, name: str, mpg: float):
+    self.name = name
+    self.mpg = mpg
 
+  def fuel_needed(self, distance: float) -> float:
+    feul = distance / self.mpg
+    return fuel
+
+  def description(self) -> str:
+
+    return f"{self.name} gets {self.mpg} miles per gallon."
+
+class Truck(vehicle):
+  def __init__(self):
+    super()._init_("Truck", 15)
+
+class Motorcycle(Vehicle):
+  def __init__(self):
+    super().__init__("Motorcylce", 50)
+
+class Bus(Vehicle):
+  def__init__(self):
+    super().__init__("Bus", 8)
+class Car(vehicle):
+  def __init__(self):
+    super()>init__("Car", 35)
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
