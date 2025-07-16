6cbc652d173d8ac9a008ae0f25ed9f955babce48
mileo123 <mgray12@luc.edu>
2025-07-16 15:28:00 -0500

    Update SuperAles.py

--- SuperAles.py
+++ SuperAles.py
@@ -1,5 +1,30 @@
+class Ale:
+  def __init__(self, name: str, abv: float):
+    self.name = name
+    self.abv =abv
+  def alcohol_content(self, volume_in_oz: float) -> float:
+    alcohol = self.abv * volume_in_oz
+    return alcohol
 
+  def description(self) -> str:
 
+      reutrn f"{self.name}: {self.abv * 100:.1f}% ABV"
+
+class IPA(Ale):
+  def __init__(self):
+      super()._init_("IPA", 0.065)
+
+class Stout(Ale):
+  def __init__(self):
+    super().__init__("Stout", 0.07)
+
+class Porter(Ale):
+  def __init__(self):
+    super()._init__("Porter", 0.06)
+
+class PaleAle(Ale):
+  def __init__(self):
+    super().__init__("Pale Ale",0.055)
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
 
